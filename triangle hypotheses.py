0153850e8e604628670078bb64f91d2103bc2214
JosephThomass <47589002+JosephThomass@users.noreply.github.com>
2020-02-21 11:03:03 +0000

    Add files via upload
    Python program to find hypotheses of a right triangle

---
+++ triangle hypotheses.py
@@ -0,0 +1,6 @@
+base = float(input('Base of triangle: '))
+perpendicular = float(input('Perpendicular of triangle: '))
+
+hypotenuse = (base**2 + perpendicular**2)**.5
+
+print('Hypotenuse:', hypotenuse)
